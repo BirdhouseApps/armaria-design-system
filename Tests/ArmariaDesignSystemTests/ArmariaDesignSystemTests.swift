2ae72238998413a69c11808c61e20c88be0ddca9
Nate Bird <natebird@gmail.com>
2020-12-05 16:11:53 -0500

    Comment out the example test.

--- Tests/ArmariaDesignSystemTests/ArmariaDesignSystemTests.swift
+++ Tests/ArmariaDesignSystemTests/ArmariaDesignSystemTests.swift
@@ -6,7 +6,7 @@ final class ArmariaDesignSystemTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(ArmariaDesignSystem().text, "Hello, World!")
+//        XCTAssertEqual(ArmariaDesignSystem().text, "Hello, World!")
     }
 
     static var allTests = [
